0d3bc8df022906dae86f5679253919f0fff95a5c
Nicolas Zanotti <nicolaszanotti@me.com>
2022-03-07 09:58:15 +0100

    fix: Projects Page - move markup from hook to render array

--- web/modules/custom/projects_page/src/Controller/ProjectsListController.php
+++ web/modules/custom/projects_page/src/Controller/ProjectsListController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\projects_page\Controller;
 
+use Drupal\Core\Url;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\projects_page\Service\ProjectListService;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,14 +22,29 @@ class ProjectsListController extends ControllerBase {
   }
 
   public function content() {
-    return [
-      '#markup' => '<div id="app"></div>',
-      '#attached' => [
-        'library' => [
-          'projects_page/dist',
-        ],
+    $page = [];
+
+    // Preload the JSON and include it in the Drupal settings JavaScript object
+    $jsonEndpointUrl = Url::fromRoute('projects_page.projects.json')->toString();
+    $preload_json = [
+      '#tag' => 'link',
+      '#attributes' => [
+        'rel' => 'preload',
+        'href' => $jsonEndpointUrl,
+        'as' => 'fetch',
+        'crossorigin' => '',
       ],
     ];
+    $page['#attached']['html_head'][] = [$preload_json, 'preload_json'];
+    $page['#attached']['drupalSettings']['projects_page']['endpoint'] = $jsonEndpointUrl;
+
+    $page['#attached']['library'][] = 'projects_page/dist';
+    $page['#attached']['library'][] = 'core/drupalSettings';
+
+    $page['#markup'] = '<div id="app"></div>';
+    $page['#cache']['max-age'] = 31536000;
+
+    return $page;
   }
 
   public function json() {
