d13cfee3d5f2bc0e28b6e5f6596444bb39692413
eddie ruxton <ae.ruxton@gmail.com>
2022-03-09 11:20:10 +0000

    tag input component brought in, not yet wired up

---
+++ web/modules/custom/projects_page/app/components/TagInput.jsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useRef } from 'react'
+import ReactTags from 'react-tag-autocomplete'
+
+const TagInput = ({ setTags, tags }) => {
+  
+  const suggestions = [
+    { id: 1, name: "Human rights" },
+    { id: 2, name: "Food" },
+    { id: 7, name: "Education" },
+    { id: 8, name: "Food" },
+  ]
+
+  const reactTags = useRef()
+
+  const onDelete = useCallback((tagIndex) => {
+    setTags(tags.filter((_, i) => i !== tagIndex))
+  }, [setTags, tags])
+
+  const onAddition = useCallback((newTag) => {
+    setTags([...tags, newTag])
+  }, [setTags, tags])
+
+  const tagComponent = ({ tag, removeButtonText, onDelete }) => {
+    return (
+      <button type='button' title={removeButtonText} onClick={onDelete}>
+        {tag.name}
+      </button>
+    )
+  }
+  
+  return (
+    <ReactTags
+      ref={reactTags}
+      tags={tags}
+      suggestions={suggestions}
+      onDelete={onDelete}
+      onAddition={onAddition}
+      tagComponent={tagComponent}
+    />
+  )
+}
+
+export default TagInput
