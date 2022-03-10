bb7b0158e6da2a963619df26b5aa9420e128e2f1
eddie ruxton <ae.ruxton@gmail.com>
2022-03-10 11:43:18 +0000

    sdg data array now uses png files instead of urls, each sdg now has an id

--- web/modules/custom/projects_page/app/components/SDGdata.jsx
+++ web/modules/custom/projects_page/app/components/SDGdata.jsx
@@ -1,109 +1,129 @@
-// import sdg01 from '../images/sdg01.png'
-// import sdg02 from '../images/sdg02.png'
-// import sdg03 from '../images/sdg03.png'
-// import sdg04 from '../images/sdg04.png'
-// import sdg05 from '../images/sdg05.png'
-// import sdg06 from '../images/sdg06.png'
-// import sdg07 from '../images/sdg07.png'
-// import sdg08 from '../images/sdg08.png'
-// import sdg09 from '../images/sdg09.png'
-// import sdg10 from '../images/sdg10.png'
-// import sdg11 from '../images/sdg11.png'
-// import sdg12 from '../images/sdg12.png'
-// import sdg13 from '../images/sdg13.png'
-// import sdg14 from '../images/sdg14.png'
-// import sdg15 from '../images/sdg15.png'
-// import sdg16 from '../images/sdg16.png'
-// import sdg17 from '../images/sdg17.png'
-
-// temporary solution - use urls instead of pngs
-const baseURL = 'https://www.un.org/sustainabledevelopment/wp-content/uploads/2019/08/E-Goal-'
-const endURL = '-1024x1024.png'
-const sdg01 = `${baseURL}01${endURL}`
-const sdg02 = `${baseURL}02${endURL}`
-const sdg03 = `${baseURL}03${endURL}`
-const sdg04 = `${baseURL}04${endURL}`
-const sdg05 = `${baseURL}05${endURL}`
-const sdg06 = `${baseURL}06${endURL}`
-const sdg07 = `${baseURL}07${endURL}`
-const sdg08 = `${baseURL}08${endURL}`
-const sdg09 = `${baseURL}09${endURL}`
-const sdg10 = `${baseURL}10${endURL}`
-const sdg11 = `${baseURL}11${endURL}`
-const sdg12 = `${baseURL}12${endURL}`
-const sdg13 = `${baseURL}13${endURL}`
-const sdg14 = `${baseURL}14${endURL}`
-const sdg15 = `${baseURL}15${endURL}`
-const sdg16 = `${baseURL}16${endURL}`
-const sdg17 = `${baseURL}17${endURL}`
+import sdg01 from '../images/sdg01.png'
+import sdg02 from '../images/sdg02.png'
+import sdg03 from '../images/sdg03.png'
+import sdg04 from '../images/sdg04.png'
+import sdg05 from '../images/sdg05.png'
+import sdg06 from '../images/sdg06.png'
+import sdg07 from '../images/sdg07.png'
+import sdg08 from '../images/sdg08.png'
+import sdg09 from '../images/sdg09.png'
+import sdg10 from '../images/sdg10.png'
+import sdg11 from '../images/sdg11.png'
+import sdg12 from '../images/sdg12.png'
+import sdg13 from '../images/sdg13.png'
+import sdg14 from '../images/sdg14.png'
+import sdg15 from '../images/sdg15.png'
+import sdg16 from '../images/sdg16.png'
+import sdg17 from '../images/sdg17.png'
 
 export const sdgData = [
   {
     imgSrc: sdg01,
-    goal: 'No Poverty'
+    goal: 'No Poverty',
+    id: '01'
   },
   {
     imgSrc: sdg02,
     goal: 'Zero Hunger'
+    ,
+    id: '02'
   },
   {
     imgSrc: sdg03,
-    goal: 'Good Health and Well-being'
+    goal: 'Good Health and Well-being',
+    id: '03'
   },
   {
     imgSrc: sdg04,
-    goal: 'Quality Education'
+    goal: 'Quality Education',
+    id: '04'
   },
   {
     imgSrc: sdg05,
-    goal: 'Gender Equality'
+    goal: 'Gender Equality',
+    id: '05'
   },
   {
     imgSrc: sdg06,
-    goal: 'Clean Water and Sanitation'
+    goal: 'Clean Water and Sanitation',
+    id: '06'
   },
   {
     imgSrc: sdg07,
-    goal: 'Affordable and Clean Energy'
+    goal: 'Affordable and Clean Energy',
+    id: '07'
   },
   {
     imgSrc: sdg08,
-    goal: 'Decent Work and Economic Growth'
+    goal: 'Decent Work and Economic Growth',
+    id: '08'
   },
   {
     imgSrc: sdg09,
-    goal: 'Industry, Innovation and Infrastructure'
+    goal: 'Industry, Innovation and Infrastructure',
+    id: '09'
   },
   {
     imgSrc: sdg10,
-    goal: 'Reduced Inequality'
+    goal: 'Reduced Inequality',
+    id: '10'
   },
   {
     imgSrc: sdg11,
-    goal: 'Sustainable Cities and Communities'
+    goal: 'Sustainable Cities and Communities',
+    id: '11'
   },
   {
     imgSrc: sdg12,
-    goal: 'Responsible Consumption and Production'
+    goal: 'Responsible Consumption and Production',
+    id: '12'
   },
   {
     imgSrc: sdg13,
-    goal: 'Climate Action'
+    goal: 'Climate Action',
+    id: '13'
   },
   {
     imgSrc: sdg14,
-    goal: 'Life Below Water'
+    goal: 'Life Below Water',
+    id: '14'
   },
   {
     imgSrc: sdg15,
-    goal: 'Life on Land'
+    goal: 'Life on Land',
+    id: '15'
   },
   {
     imgSrc: sdg16,
-    goal: 'Peace and Justice Strong Institutions'
+    goal: 'Peace and Justice Strong Institutions',
+    id: '16'
   },
   {
     imgSrc: sdg17,
-    goal: 'Partnerships to achieve the Goal'
+    goal: 'Partnerships to achieve the Goal',
+    id: '17'
   },
 ]
+
+
+
+// temporary solution - use urls instead of pngs
+// const baseURL = 'https://www.un.org/sustainabledevelopment/wp-content/uploads/2019/08/E-Goal-'
+// const endURL = '-1024x1024.png'
+// const sdg01 = `${baseURL}01${endURL}`
+// const sdg02 = `${baseURL}02${endURL}`
+// const sdg03 = `${baseURL}03${endURL}`
+// const sdg04 = `${baseURL}04${endURL}`
+// const sdg05 = `${baseURL}05${endURL}`
+// const sdg06 = `${baseURL}06${endURL}`
+// const sdg07 = `${baseURL}07${endURL}`
+// const sdg08 = `${baseURL}08${endURL}`
+// const sdg09 = `${baseURL}09${endURL}`
+// const sdg10 = `${baseURL}10${endURL}`
+// const sdg11 = `${baseURL}11${endURL}`
+// const sdg12 = `${baseURL}12${endURL}`
+// const sdg13 = `${baseURL}13${endURL}`
+// const sdg14 = `${baseURL}14${endURL}`
+// const sdg15 = `${baseURL}15${endURL}`
+// const sdg16 = `${baseURL}16${endURL}`
+// const sdg17 = `${baseURL}17${endURL}`
