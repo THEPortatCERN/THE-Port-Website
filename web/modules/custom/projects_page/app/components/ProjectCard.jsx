a803847c025009a9ae5916e7977ca41962b34090
eddie ruxton <ae.ruxton@gmail.com>
2022-03-15 13:48:49 +0000

    bug fix, team names now remain on cards when filtering projects

--- web/modules/custom/projects_page/app/components/ProjectCard.jsx
+++ web/modules/custom/projects_page/app/components/ProjectCard.jsx
@@ -34,7 +34,7 @@ const ProjectCard = ({
         )}
       </div>
       <div className="card-body">
-        <p className="card-text">{teams.pop(", ")}</p>
+        <p className="card-text">{teams.join(', ')}</p>
         <h3 className="card-title">{title}</h3>
         <p className="card-text">{summary}</p>
         <a href={page_url} className="card-link">
@@ -42,10 +42,10 @@ const ProjectCard = ({
         </a>
         <ul>
           {attributes.map((attribute, index) => (
-            <li className="badge bg-secondary" key={index} >{attribute}</li>
+            <li className="badge" key={index} >{attribute}</li>
           ))}
         </ul>
-        <ul className='card-sdgs'>
+        <ul>
           {sdgNums.map((num, index) => (
             <li key={index}>
               <img src={`https://www.un.org/sustainabledevelopment/wp-content/uploads/2019/08/E-Goal-${num}-1024x1024.png`}/>
