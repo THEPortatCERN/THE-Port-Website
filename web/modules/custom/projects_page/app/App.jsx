480d381577f131a38992bef3cf2cebaabfb6757f
eddie ruxton <ae.ruxton@gmail.com>
2022-03-28 13:11:23 +0100

    redo clear-filters-button

--- web/modules/custom/projects_page/app/App.jsx
+++ web/modules/custom/projects_page/app/App.jsx
@@ -33,38 +33,55 @@ function App() {
     : projectList  
   //------------------------------------------------------------------//
 
+  const onClearFitlers = () => {
+    setTags([])
+    setChosenSDG('')
+    setTeamSearch('')
+    setTitleSearch('')
+    setEventSearch('')
+  }
+
   return (
-   <div className="projects-and-filters">
-     <div className="limit-search">
-      <TeamSearch setTeamSearch={setTeamSearch} />
-      <TitleSearch setTitleSearch={setTitleSearch}/>
-      <EventSearch setEventSearch={setEventSearch}/>
-      <button type="button" className="btn filter-button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">More filters</button>
-     </div>
-    {/* --------------------------------------------------- filter section ------------------------------------------------------- */}
-    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-      <div className="offcanvas-header">
-        <h5 id="offcanvasRightLabel">Choose Filters</h5>
-        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div className="offcanvas-body filters-section">
-        <TagInput tags={tags} setTags={setTags} className='single-filter-system'/>
-        <SDGlist chosenSDG={chosenSDG} setChosenSDG={setChosenSDG} className='single-filter-system'/>
-      </div>
-    </div>
-    {/* --------------------------------------------------- project list ----------------------------------------------------------- */}
-    <div className="projects">
-      <div className="view-content">
+    <div className="projects-and-filters">
+      <div className="limit-search">
+        <TeamSearch setTeamSearch={setTeamSearch} />
+        <TitleSearch setTitleSearch={setTitleSearch}/>
+        <EventSearch setEventSearch={setEventSearch}/>
+        <button type="button" className="btn filter-button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            More filters
+        </button>
         {
-          filteredList.length > 0 ?
-          filteredList.map((project, index) => (
-            <ProjectCard {...project} key={index}/>
-          ))
-          : (<h2>Currently, no projects match your search.</h2>)
+          (tags.length > 0) || (chosenSDG.length > 0) ?
+          <button type="button" className="clear-filters-button" onClick={onClearFitlers}>
+            Clear Filters
+          </button>
+          : <></>
         }
       </div>
+      {/* --------------------------------------------------- filter section ------------------------------------------------------- */}
+      <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div className="offcanvas-header">
+          <h5 id="offcanvasRightLabel">Choose Filters</h5>
+          <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div className="offcanvas-body filters-section">
+          <TagInput tags={tags} setTags={setTags} className='single-filter-system'/>
+          <SDGlist chosenSDG={chosenSDG} setChosenSDG={setChosenSDG} className='single-filter-system'/>
+        </div>
+      </div>
+      {/* --------------------------------------------------- project list ----------------------------------------------------------- */}
+      <div className="projects">
+        <div className="view-content">
+          {
+            filteredList.length > 0 ?
+            filteredList.map((project, index) => (
+              <ProjectCard {...project} key={index}/>
+            ))
+            : (<h2>Currently, no projects match your search.</h2>)
+          }
+        </div>
+      </div>
     </div>
-   </div>
   );
 }
 
